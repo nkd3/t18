050d550374c0465acabb28b0833dccd4502fb627
nkd <neelkanth.dwibedi@gmail.com>
2025-10-14 12:30:11 +0100

    auto-sync: 2025-10-14 12:30:11

---
+++ tools/start-watch-and-sync.ps1
@@ -0,0 +1,5 @@
+﻿Start-Process -WindowStyle Hidden -FilePath "powershell.exe" -ArgumentList @(
+  "-NoProfile",
+  "-ExecutionPolicy","Bypass",
+  "-File","C:\T18\tools\watch-and-sync.ps1"
+) | Out-Null
